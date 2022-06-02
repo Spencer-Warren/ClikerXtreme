a15d4c0d0b06b9952438b0b57535f9b986ffce7f
Valereon <95433166+Valereon@users.noreply.github.com>
2022-06-02 12:39:04 -0400

    I tried to add functionality to the buttons but i cant figure out your update loop

--- src/main.js
+++ src/main.js
@@ -13,25 +13,26 @@ function update(time) {
     for(const [key, value] of Object.entries(items)) value.update()
 
     lastTime = time
+    
     window.requestAnimationFrame(update)    
 }
 
 window.requestAnimationFrame(update)
 // init rocks
-var itemNames = ['rock', "diamond", "Stick", "table"];
+var itemNames = ['Rock', "Stick", "Sand"];
 var items = {};
 for(const name of itemNames){
     items[name] = new Item(name);
     document.getElementById(name).addEventListener("click", function() {items[name].count +=1});
 }
-btn1 = new upgradeButton("Rocks ", 100, " Miner ", 2)
+btn1 = new upgradeButton("Rocks ", 10, " Miner ", 2)
 var upgradeNames = [btn1]
 for(const btn of upgradeNames){
     document.getElementById(name).addEventListener("click", function(){
-        if(item.count >= btn.price){
+        if(Item.count >= btn.price){
             btn.amount += 1
-            btn.priceMult 
-
+            btn.priceMult
+            btn.price -= Item.count
         }
     })
 }
