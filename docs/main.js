52a2d4bbafae04794fc1b956a7934fd19aa00eb4
Spencer A Warren <spencer.warren.19@cnu.edu>
2022-06-04 15:41:58 -0400

    What did i chag

--- docs/main.js
+++ docs/main.js
@@ -4,8 +4,8 @@ import { saveIO } from './Classes/SaveIO.js'
 // init items
 var itemNames = ["Rock", "Stick", "Sand"];
 var items = [
-    new Item("Rock"), 
-    new Item("Stick"), 
+    new Item("Rock"),
+    new Item("Stick"),
     new Item("Sand"),
 ];
 var stations = [
@@ -18,30 +18,28 @@ var allItems = items.concat(stations);
 
 for (const item of items) {
     document.getElementById(item.name + "-button")
-    .addEventListener("click", function() { item.add(1) });
+        .addEventListener("click", function () { item.add(1) });
 }
 
 //add event listener
 for (const station of stations) {
-    document.getElementById(station.name+"-button")
-        .addEventListener("click", function() {
+    document.getElementById(station.name + "-button")
+        .addEventListener("click", function () {
             if (station.item.count >= station.price) {
-                station.count += 1
-                station.item.count -= station.price
-                station.update()
-                station.item.update()
+                station.add(1);
+                station.item.add(-station.price);
             }
         })
 }
 
 var save = new saveIO(allItems);
-document.getElementById("save").addEventListener("click", function(){save.save()});
-document.getElementById("load").addEventListener("click", function(){save.load()});
+document.getElementById("save").addEventListener("click", function () { save.save() });
+document.getElementById("load").addEventListener("click", function () { save.load() });
 
 // Auto load on page load
 document.addEventListener("DOMContentLoaded", save.load());
 // save every so often
-setInterval(function() {
+setInterval(function () {
     // save.save();
     console.log(" ~ Auto Saved ~ ");
 }, 45000);
