5e3948c1379d825bda0e74749700c84f023f32ea
Spencer A Warren <spencer.warren.19@cnu.edu>
2022-06-03 21:24:55 -0400

    Change local storage check

--- docs/main.js
+++ docs/main.js
@@ -47,8 +47,9 @@ function save() {
 }
 
 function load() {
-    if (localStorage.length > 0) {
-        for (const name of itemNames) {
+    for (const name of itemNames) {
+        // if item doesnt exist in local storage ignore
+        if (localStorage.getItem(name) === null) {
             var amount = JSON.parse(localStorage.getItem(name))
             items[name].set(amount);
         }
