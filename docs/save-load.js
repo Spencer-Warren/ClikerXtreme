48ff6db509fb22413faa3a1c0c2ec16ba1bb2fae
Spencer A Warren <spencer.warren.19@cnu.edu>
2022-06-03 20:56:38 -0400

    Fix var name in save load

--- docs/save-load.js
+++ docs/save-load.js
@@ -1,20 +1,20 @@
-export class autoSave{
-    constructor(ID){
-    
-    this.saveButton = document.createElement("button")
-    this.button.id = ID
-    this.saveButtonName = this.saveButton.innerHTML = "Save"
-    
-    document.getElementById("content").appendChild(this.saveButton)
-    
-    
+export class autoSave {
+    constructor(ID) {
+
+        this.saveButton = document.createElement("button")
+        this.saveButton.id = ID
+        this.saveButtonName = this.saveButton.innerHTML = "Save"
+
+        document.getElementById("content").appendChild(this.saveButton)
+
+
     }
 }
-export class autoLoad{
-constructor(ID){
-    this.buttonid = ID
-    this.loadButton = document.createElement("button")
-    this.loadButtonName = this.resetButton.innerHTML = "Load"
-    document.getElementById("content").appendChild(this.resetButton)
+export class autoLoad {
+    constructor(ID) {
+        this.buttonid = ID
+        this.loadButton = document.createElement("button")
+        this.loadButtonName = this.resetButton.innerHTML = "Load"
+        document.getElementById("content").appendChild(this.resetButton)
     }
 }
