7b0029abd31fddb40b737642087b0907af5d5827
Valereon <95433166+Valereon@users.noreply.github.com>
2022-10-06 11:11:50 -0400

    MADE AUTO STATIONS WORK

--- docs/Classes/Station.js
+++ docs/Classes/Station.js
@@ -8,18 +8,21 @@ export class Station extends BasicItem {
         this.item = item;
 
         // Multiplies the price
-        this.priceMult = mult *= price;
+        this.price = mult * price;
 
         // Button to buy upgrade
         this.button.innerHTML = `Buy 1 ${this.name} - ${this.price}  ${item.name}`;
 
         // init inner html
         super.update();
+
+        //add a interval to add items
+        setInterval(Station.increment, 1000, this);
     }
 
     static increment(station){
         console.log(station.item.count);
         station.item.add(station.count);
     }
-
+      
 }
